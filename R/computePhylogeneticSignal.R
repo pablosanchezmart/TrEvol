e8bb7d51d7e66b122d1bfea0ccf29df38d5d4c68
Pablo Sanchez-Martinez <p.sanchez@creaf.uab.cat>
2022-05-03 09:44:46 +0200

    computePhylogeneticSignal function.

--- R/computePhylogeneticSignal.R
+++ R/computePhylogeneticSignal.R
@@ -11,76 +11,60 @@
 #'
 #' @examples
 #' @importFrom rlang .data
-computePhylogeneticSignal <- function(variable, dataset, phylogeny, model.specifications = NULL, forceRun = T){
-
-
-  # Complete data
-  modellingData <- completePhyloData(phylogeny = phylogeny, dataset = dataset, traits = variable)
-  # Model formula
+computePhylogeneticSignal <- function(variable, dataset, phylogeny, model.specifications = NULL,
+                                       forceRun = T)
+{
+  modellingData <- completePhyloData(phylogeny = phylogeny,
+                                     dataset = dataset, traits = variable)
   fix.frml <- paste0(variable, " ~ 1")
-
-  # Run models if previous results do not exist
-  if(!file.exists(paste0(outputs.dir, "/models_outputs/phyloSignalResults.RData")) | forceRun){
-
-    # Model specifications
-    if(is.null(model.specifications)){
+  if (!file.exists(paste0(outputs.dir, "/models_outputs/phylo_signal.RData")) |
+      forceRun) {
+    if (is.null(model.specifications)) {
       print("Using default model specificatios. Use defineModelsSpecifications() output on model.specifications argument to set them manually.")
       model.specifications <- defineModelsSpecifications()
-      # print(model.specifications)
+    }
+    mdl <- MCMCglmm::MCMCglmm(stats::as.formula(fix.frml),
+                              random = ~animal, family = "gaussian", prior = model.specifications$uniresponse_prior,
+                              data = modellingData$dta, pedigree = modellingData$phylo,
+                              nitt = model.specifications$number_interations, burnin = model.specifications$burning_iterations,
+                              thin = model.specifications$thinning_iterations,
+                              verbose = F)
+    mdl$name <- fix.frml
+  }
+  else {
+    print("loanding previous results")
+    load(file = paste0(outputs.dir, "/models_outputs/phylo_signal.RData"),
+         envir = .GlobalEnv)
+    if(length(phylo.signal.results$phyloSignal[,1])){
+      mdl <- phylo.signal.results$model
+    } else{
+      mdl <- phylo.signal.results$individual.models.results[[which(!is.na(stringr::str_extract(names(phylo.signal.results$individual.models.results),
+                                                                                               variable)))]]$model
     }
 
-    # Model
-    mdl <- MCMCglmm::MCMCglmm(stats::as.formula(fix.frml), random = ~ animal,
-                              family = "gaussian",
-                              prior = model.specifications$uniresponse_prior,
-                              data = modellingData$dta,
-                              pedigree = modellingData$phylo,
-                              nitt = model.specifications$number_interations,
-                              burnin = model.specifications$burning_iterations,
-                              thin = model.specifications$thinning_iterations,
-                              verbose=F)
-    mdl$name <- fix.frml
-
-  } else {
-    print("loanding previous results")
-    load(file = paste0(.data$outputs.dir, "/models_outputs/phyloSignalResults.RData"),  envir = .GlobalEnv)
-    mdl <- .data$phylo.signal.results$individual.models.results[[which(!is.na(stringr::str_extract(names(phylo.signal.results$individual.models.results), variable)))]]$model
   }
-
-  # Bayesian model diagnostics check
   model.diagnostics <- diagnoseModels(model = mdl)
-
-  # Phylo. signal using phytools
-
   var.df <- modellingData$dta[, variable]
   names(var.df) <- modellingData$dta$animal
+  k <- as.numeric(phytools::phylosig(modellingData$phylo, var.df,
+                                     method = "K"))
+  lambda <- as.numeric(phytools::phylosig(modellingData$phylo,
+                                          var.df, method = "lambda")[1])
+  phylo.signal.results <- list()
+  phylo.signal.results$model <- mdl
+  phylo.signal.results$wlambda.distr <- mdl$VCV[, "animal"]/(mdl$VCV[,
+                                                                     "animal"] + mdl$VCV[, "units"])
+  wlambda <- mean(phylo.signal.results$wlambda.distr)
+  phylo.signal.results$resVar.distr <- mdl$VCV[, "units"]/(mdl$VCV[,
+                                                                   "animal"] + mdl$VCV[, "units"])
+  residualVariance <- mean(phylo.signal.results$resVar.distr)
+  phylo.signal.results$phyloSignal <- data.frame(Variable = variable, N = length(modellingData$dta$animal),
+                                                 Model = fix.frml, K = k, Lambda = lambda, Wlambda = wlambda,
+                                                 Non_Phylogenetic_variance = residualVariance)
+  phylo.signal.results$model.diagnostics <- model.diagnostics
 
-  k <- as.numeric(phytools::phylosig(modellingData$phylo, var.df, method = "K"))
-  lambda <- as.numeric(phytools::phylosig(modellingData$phylo, var.df, method = "lambda")[1])
-
-  ## Results gathering
-  rslts <- list()
-  rslts$model <- mdl
-
-  # Weighted lambda from mcmcglmm results
-  rslts$wlambda.distr <- mdl$VCV[,"animal"]/ (mdl$VCV[, "animal"] + mdl$VCV[, "units"])
-  wlambda <- mean(rslts$wlambda.distr)
-
-  # Weighted residual variance from mcmcglmm results
-  rslts$resVar.distr <- mdl$VCV[, "units"]/ (mdl$VCV[, "animal"] + mdl$VCV[, "units"])
-  residualVariance <- mean(rslts$resVar.distr)
-
-  # Phylogenetic signal data frame
-  rslts$phyloSignal <- data.frame("Variable" = variable,
-                                  "N" = length(modellingData$dta$animal),
-                                  "Model" = fix.frml,
-                                  "K" = k,
-                                  "Lambda" = lambda,
-                                  "Wlambda" = wlambda,
-                                  "Non_Phylogenetic_variance" = residualVariance)
-
-  # Model diagnostics
-  rslts$model.diagnostics <- model.diagnostics
+  save(phylo.signal.results, file = paste0(outputs.dir, "/models_outputs/phylo_signal.RData"))
+  print(paste0(outputs.dir, "/models_outputs/phylo_signal.RData"))
 
   return(rslts)
 }
