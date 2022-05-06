72e3d5b2298be917df396ec35f9408b52d68ee30
Pablo Sanchez-Martinez <p.sanchez@creaf.uab.cat>
2022-05-06 12:09:37 +0200

    minor.

--- R/partialCorrelationsTraits.R
+++ R/partialCorrelationsTraits.R
@@ -73,9 +73,9 @@ partialCorrelationsTraits <- function (VARIABLES, PREDICTORS, PHYLOGENY, DATASET
 
       mdl.rslts <- computePartialCorrelations(variable1 = model.descr$resp_var1, variable2 = model.descr$resp_var2, predictors = PREDICTORS, dataset = DATASET, phylogeny = PHYLOGENY,
                                               model.specifications = MODEL.SPECIFICATIONS)
-      partialCorrelationsResults$correlation.results <- rbind(correlationsResults$correlation.results,
+      partialCorrelationsResults$correlation.results <- rbind(partialCorrelationsResults$correlation.results,
                                                        mdl.rslts$corrrelationsSummary)
-      partialCorrelationsResults$models.diagnostics <- rbind(correlationsResults$models.diagnostics,
+      partialCorrelationsResults$models.diagnostics <- rbind(partialCorrelationsResults$models.diagnostics,
                                                       mdl.rslts$model.diagnostics)
       partialCorrelationsResults$individual.models.results[[model]] <- mdl.rslts
     }
