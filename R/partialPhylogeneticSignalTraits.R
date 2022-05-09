043c103f3feb92472594ddac0f80f99e0e1f3496
Pablo Sanchez-Martinez <p.sanchez@creaf.uab.cat>
2022-05-09 15:43:36 +0200

    minor

--- R/partialPhylogeneticSignalTraits.R
+++ R/partialPhylogeneticSignalTraits.R
@@ -61,7 +61,7 @@ partialPhylogeneticSignalTraits <- function (VARIABLES, PREDICTORS, PHYLOGENY, D
   assign(paste0("partialPhylogeneticSignalResults", PREDICTORS), partialPhylogeneticSignalResults)
 
 
-  save(paste0("partialPhylogeneticSignalResults", PREDICTORS), file = paste0(outputs.dir,
+  save(get(paste0("partialPhylogeneticSignalResults", PREDICTORS)), file = paste0(outputs.dir,
                                                        "/models_outputs/partialPhylogeneticSignalResults", PREDICTORS,
                                                        ".RData"))
   print(paste0(outputs.dir, "/models_outputs/partialPhylogeneticSignalResults",
