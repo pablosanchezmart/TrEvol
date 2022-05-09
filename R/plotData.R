503a858249b15857b8361621d6a4db103e1894dc
Pablo Sanchez-Martinez <p.sanchez@creaf.uab.cat>
2022-05-09 10:45:38 +0200

    plotData function.

--- R/plotData.R
+++ R/plotData.R
@@ -12,58 +12,50 @@
 #' @export
 #'
 #' @examples
-plotData <- function(phylogeny, dataset, variables, colorBy = NULL, nameBy = NULL, panelSpace = 4, FontsizeFactor = 0.1){
-
-  if(!requireNamespace("ggtree",  quietly = TRUE)){
+plotData <- function (phylogeny, dataset, variables, colorBy = NULL, nameBy = NULL,
+                      panelSpace = 4, FontsizeFactor = 0.1) {
+  if (!requireNamespace("ggtree", quietly = TRUE)) {
     BiocManager::install("ggtree", update = F, ask = F)
   }
 
   dataset <- dataset[stats::complete.cases(dataset[, variables]), ]
   phylogeny <- ape::drop.tip(phylogeny, phylogeny$tip.label[!phylogeny$tip.label %in% dataset$animal])
   dataset <- as.data.frame(dataset)
-  row.names(dataset) <- dataset$genus
+  row.names(dataset) <- dataset$animal
   dataset <- dataset[phylogeny$tip.label, ]
-  dataset <- dataset[, c("genus", names(dataset))] # we need it as a first column
+  dataset <- dataset[, c("animal", variables)]
 
+  p <- ggtree::ggtree(phylogeny) +  ggplot2::coord_cartesian(clip = "off")
 
-  p <- ggtree::ggtree(phylogeny)
-
-  if(!is.null(nameBy)){
+  if (!is.null(nameBy)) {
     taxonList <- unique(dataset[, nameBy])
     taxonList <- taxonList[which(!is.na(taxonList))]
-
-    # name orders
-    for(o in taxonList){
+    for (o in taxonList) {
       spp <- rownames(dataset[which(dataset[, nameBy] == o), ])
-      if(length(spp) == 1){                                               # Some orders have only one species, so MRCA cannot be found
+      if (length(spp) == 1) {
         nd <- which(phylogeny$tip.label == spp)
-      } else{
+      }
+      else {
         nd <- phytools::findMRCA(phylogeny, spp, type = "node")
       }
-      p <- p + ggtree::geom_cladelabel(node= nd, label= o, fontsize = length(rownames(dataset[dataset$order == o, ])) * FontsizeFactor + 1) +
-        ggplot2::coord_cartesian(clip = 'off')  + ggtree::theme_tree2(plot.margin= ggtree::margin(6, 6, 6, 6))
+
+      p <- p + ggtree::geom_cladelabel(node = nd, label = o, fontsize = length(rownames(dataset[dataset[, nameBy] == o, ])) * FontsizeFactor + 1) +
+        ggtree::theme_tree2(plot.margin = ggtree::margin(6, 6, 6, 6))
     }
   }
 
-  if(!is.null(colorBy)){
+  if (!is.null(colorBy)) {
     cat.df <- data.frame(as.factor(dataset[, colorBy]))
-
-    for(var in variables){
-      d <- data.frame(id = row.names(dataset), varX = dataset[, var], order = cat.df[, 1])
-      p <- ggtree::facet_plot(p, panel = var, data = d, geom = ggstance::geom_barh,
-                      mapping =  aes(x = varX, fill = order),
-                      stat = "identity")
+    for (var in variables) {
+      d <- data.frame(id = row.names(dataset), varX = dataset[, var], color = cat.df[, 1])
+      p <- ggtree::facet_plot(p, panel = var, data = d, geom = ggstance::geom_barh, mapping = ggtree::aes(x = varX, fill = color), stat = "identity")
     }
   } else {
-    for(var in variables){
-      d <- data.frame(id = row.names(dataset), varX = dataset[, var], order = cat.df[, 1])
-      p <- ggtree::facet_plot(p, panel = var, data = d, geom = ggstance::geom_barh,
-                      mapping =  aes(x = varX, fill = order),
-                      stat = "identity")
+    for (var in variables) {
+      d <- data.frame(id = row.names(dataset), varX = dataset[, var])
+      p <- ggtree::facet_plot(p, panel = var, data = d, geom = ggstance::geom_barh, mapping = ggtree::aes(x = varX), stat = "identity")
     }
   }
-
-  p <- p  +  ggplot2::scale_fill_viridis_d() +
-    ggplot2::theme(panel.spacing = unit(panelSpace, "lines")) +  ggplot2::theme(legend.position = "none")
+  p <- p + ggplot2::scale_fill_viridis_d() + ggplot2::theme(panel.spacing = ggtree::unit(panelSpace, "lines")) + ggplot2::theme(legend.position = "none")
   print(p)
 }
