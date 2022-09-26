044cfcc995a96bdb6a62ff361a599de3a298415e
Pablo Sanchez-Martinez <p.sanchez@creaf.uab.cat>
2022-09-26 16:21:57 +0100

    avoid rownames in completePhyloData() and avoid family as a column name (problem with mcmcglmm),

--- R/completePhyloData.R
+++ R/completePhyloData.R
@@ -16,8 +16,20 @@ completePhyloData <- function(phylogeny, dataset, traits, taxonName = "animal") 
 
   phylo <- ape::keep.tip(phylogeny, phylogeny$tip.label[phylogeny$tip.label %in% completeData[, taxonName]])
   phyData$dta <- as.data.frame(completeData[completeData[, taxonName] %in% phylo$tip.label, ])
-  rownames(phyData$dta) <- phyData$dta[, taxonName]
-  phyData$dta <- phyData$dta[phylo$tip.label, ]
+
+  taxon_ordered <- data.frame(n = 1:length(phylo$tip.label))
+  taxon_ordered[, taxonName] <- phylo$tip.label
+  taxon_ordered$n <- NULL
+
+  if(!is.null(phylo$tip.nodes)){
+    phylo$tip.nodes <- NULL
+  }
+
+  if("family" %in% colnames(phyData$dta)){
+    phyData$dta <- phyData$dta %>% dplyr::rename(Family = family)
+    print("renaming family column to Family to avoid problems with MCMCglmm")
+  }
+  phyData$dta <- dplyr::left_join(taxon_ordered, phyData$dta, by= taxonName)
   phyData$phylo <- phylo
 
   return(phyData)
