24856af2db07be3190cc004cd9c6a6abc33c5d4e
Pablo Sanchez Martinez <psanche2@ed.ac.uk>
2023-09-11 16:25:45 +0100

    document meanOrMode function.

--- R/meanOrMode.R
+++ R/meanOrMode.R
@@ -1,11 +1,24 @@
-#' Compute mean if variable is numeric or mode if variable is character or factor.
+#' Return mean if variable is numeric or mode if variable is character or factor.
 #'
-#' @param x numeric, character or factor vector.
+#' @param x (*numeric*, *character* or *factor*). Vector from which the mean or mode will be computed for.
 #'
-#' @return mean or mode value
+#' @return (*numeric* for mean or *character* for mode). Mean or mode value.
 #' @export
 #'
 #' @importFrom rlang .data
+#'
+#' #' @examples
+#' \dontrun{
+#' # Calculate mean
+#'
+#' numeric.data <- c(1, 2, 2, 3)
+#' meanOrMode(numeric.data)
+#'
+#' # Calculate mean
+#'
+#' categorical.data <- c("1", "2", "2", "3")
+#' meanOrMode(categorical.data)
+#' }
 meanOrMode <- function(x){
   if(is.numeric(x) | is.double(x) | is.integer(x)){
     meanX <- mean(x, na.rm = T)
