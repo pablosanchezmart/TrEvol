4a8f7b2b97bfd2aefc11caf58601ebdc46122f45
Pablo Sanchez Martinez <psanche2@ed.ac.uk>
2023-09-14 18:09:14 +0100

    document plotNetwork.

--- R/plotNetwork.R
+++ R/plotNetwork.R
@@ -1,43 +1,79 @@
 #' Plot variance-covariance networks
 #'
-#' @param covariance.results (data frame) correlations output of computeCovariancePartition()$covarianceResults
-#' @param variance.results (data frame) phylogenetic signal as reported by computeVariancePartition()$varianceResults
-#' @param variance.type (character) variable name of the phylogenetic signal as reported in varianceResults
-#' @param covariance.type (character) variable name of the correlation as reported in correlations
-#' @param group.variables (data frame) character vector as long as the number of variables indicating the variable name in the first column and the group of the variable in the second.
-#' @param order.variables (character) order of the variables to plot
-#' @param edge.label (logical) if TRUE, edge labels show correlation coefficients
-#' @param layout "spring" or "circular"
-#' @param only.significant (logical) if TRUE, only significant correlations are plotted
-#' @param not.show.variables (character) vector of variables present in correlations data but exclued from the plot
-#' @param threshold (numeric) correlations to be shown (e.g., those with a absolute value higher than 0.1)
-#' @param label.size (numeric) size of the node labels
-#' @param node.label (data frame) character vector as long as the number of variables indicating the variable name in the first column and the new name, which will be shown in the plot.
-#' @param networkMetrixTextSize (numeric) Size of the text reporting network metrics
-#' @param displayDegreeAsNodeSize (logical) If true, node size is proportional to its degree.
+#' Plot variance-covariance partition obtained using the function computeVarianceCovariancePartition of this package.
+#'
+#' @param covariance_results (*data frame*). Correlations output of computeVarianceCovariance(). It can be found in "results_object$covarianceResults".
+#' @param variance_results (*data frame*). Variance partition as reported by computeVarianceCovariance(). It can be found in "results_object$varianceResults.
+#' @param variance_type (*character*). Name of the portion of variance to report in the nodes of the plot. When environmental variable is not included in computeVarianceCovariance(), it can be set as "phylogenetic_variance" or "non_phylogenetic_variance".
+#' If environmental variable is included, this argument can be set as "non_attributed_phylogenetic_variance", "environmental_phylogenetic_variance", "labile_environmental_variance" and "residual_variance".
+#' @param correlation_type (*character*). Name of the portion of the correlation to report in the edges of the plot. When environmental variable is not included in computeVarianceCovariance(), it can be set as "total_correlation", "phylogenetic_correlation" or "non_phylogenetic_correlation".
+#' If environmental variable is included, this argument can be set as "non_attributed_phylogenetic_correlation", "environmental_phylogenetic_correlation", "labile_environmental_correlation" and "residual_correlation".
+#' @param group_variables (*data frame*). Data frame wich a first column indicating the variables to be plotted included in covariance_results and varaince_results and the group of the variable in the second column. Variables of the same group will be ploted with the same colour.
+#' @param order_variables (*character*). Character indicating the variables names to be plotted in the desired order. The variables will be plotted in the desired order clockwise, when layout is set to be circular.
+#' @param edge_label (*logical*). If TRUE, edge labels show correlation coefficients. Default set to FALSE.
+#' @param layout (*"spring"*, or *"circular"*). Network layout (default as circular). See qgraph documentation for further information.
+#' @param only_significant (*logical*). if TRUE (default), only significant correlations are represented.
+#' @param exclude_variables (*character*). Vector of variables present in correlations data but excluded from the plot. It can be useful to plot subsets of the results.
+#' @param correlation_threshold (*numeric*). Only correlation with an absolute value higher than the threshold will be plotted. When set to zero, there is no threshold (default).
+#' @param label_size (*numeric*). Control the size of the node labels.
+#' @param node_label (*data frame*). Data frame indicating the variable name in the first column and the new name, which will be shown in the plot, in the second column.
+#' @param plot_metrics (*logical*). If true, network metrics are displayed.
+#' @param network_metrics_text_size (*numeric*). Size of the text reporting network metrics.
+#' @param degree_as_node_size (*logical*) If TRUE (default), node size is proportional to the degree of the variable (number of edges connected to a given node representing a variable).
 #'
 #' @return
 #' @export
 #'
 #' @examples
-plotNetwork <- function (covariance.results, variance.results = NULL, variance.type = NULL,
-                         covariance.type, group.variables = NULL, order.variables = NULL, edge.label = F,
-                         layout = "circular", only.significant = T, not.show.variables = NULL,
-                         threshold = 0, label.size = 0.8, node.label = NULL, plot.metrics = T,
-                         networkMetrixTextSize = 1, displayDegreeAsNodeSize = T)
+#' \dontrun{
+#' # Simulate example data
+#' simulated_traits.data <- simulateDataSet()
+#'
+#' # Compute variance-covariance structure for simulated traits using default parameters
+#' variance_covariance_results <- computeVarianceCovariancePartition(
+#' traits = c("phylo_G1_trait1", "phylo_G1_trait2"),
+#' environmental_variable = "phylo_G1_env",
+#' phylogeny = simulated_traits.data$phylogeny
+#' )
+#'
+#' # plot network
+#'
+#' plotNetwork(
+#' covariance_results = variance_covariance_results$covarianceResults,
+#' variance_results = variance_covariance_results$varianceResults,
+#' variance_type = environmental_phylogenetic_variance,
+#' correlation_type = environmental_phylogenetic_covariance
+#' )
+#' }
+plotNetwork <- function (covariance_results = NULL,
+                         variance_results = NULL,
+                         variance_type = NULL,
+                         correlation_type = NULL,
+                         group_variables = NULL,
+                         order_variables = NULL,
+                         edge_label = F,
+                         layout = "circular",
+                         only_significant = T,
+                         exclude_variables = NULL,
+                         correlation_threshold = 0,
+                         label_size = 0.8,
+                         node_label = NULL,
+                         plot_metrics = T,
+                         network_metrics_text_size = 1,
+                         degree_as_node_size = T)
 {
 
   ### Data preparation ####
 
   # set non significant values to zero if needed
-  if (only.significant) {
-    covariance.results[which(covariance.results[, paste0("Pvalue_", covariance.type)] > 0.05), covariance.type] <- 0
+  if (only_significant) {
+    covariance_results[which(covariance_results[, paste0("Pvalue_", correlation_type)] > 0.05), correlation_type] <- 0
   }
 
-  if (threshold > 0) {
-    covariance.results[which(abs(covariance.results[, covariance.type]) < threshold), covariance.type] <- 0
+  if (correlation_threshold > 0) {
+    covariance_results[which(abs(covariance_results[, correlation_type]) < correlation_threshold), correlation_type] <- 0
   }
-  correlations <- covariance.results[, c("Trait_1", "Trait_2", covariance.type)]
+  correlations <- covariance_results[, c("Trait_1", "Trait_2", correlation_type)]
   vars <- unique(c(correlations$Trait_1, correlations$Trait_2))
   correlation.matrix <- matrix(ncol = length(vars), nrow = length(vars), 0)
   colnames(correlation.matrix) <- vars
@@ -45,31 +81,31 @@ plotNetwork <- function (covariance.results, variance.results = NULL, variance.t
   for (i in 1:length(correlations$Trait_1)) {
     var1 <- correlations$Trait_1[i]
     var2 <- correlations$Trait_2[i]
-    correlation.matrix[var1, var2] <- correlations[i, covariance.type]
-    correlation.matrix[var2, var1] <- correlations[i, covariance.type]
+    correlation.matrix[var1, var2] <- correlations[i, correlation_type]
+    correlation.matrix[var2, var1] <- correlations[i, correlation_type]
     correlation.matrix[var1, var1] <- 0
     correlation.matrix[var2, var2] <- 0
   }
-  if (!is.null(not.show.variables) && any(not.show.variables %in% vars)) {
-    vars <- vars[-which(vars %in% not.show.variables)]
+  if (!is.null(exclude_variables) && any(exclude_variables %in% vars)) {
+    vars <- vars[-which(vars %in% exclude_variables)]
     correlation.matrix <- correlation.matrix[vars, vars]
   }
-  if (!is.null(group.variables)) {
-    if (all(vars %in% group.variables[, 1])) {
-      group.variables <- group.variables[which(group.variables[, 1] %in% vars),
+  if (!is.null(group_variables)) {
+    if (all(vars %in% group_variables[, 1])) {
+      group_variables <- group_variables[which(group_variables[, 1] %in% vars),
       ]
-      correlation.matrix <- correlation.matrix[group.variables[, 1], group.variables[, 1]]
-      vars <- group.variables[, 1]
+      correlation.matrix <- correlation.matrix[group_variables[, 1], group_variables[, 1]]
+      vars <- group_variables[, 1]
     } else {
-      stop("Not all variables of interest are included in group.variables argument.")
+      stop("Not all variables of interest are included in group_variables argument.")
     }
   }
-  if (!is.null(order.variables)) {
-    if (all(vars %in% order.variables)) {
-      vars <- order.variables[which(order.variables %in% vars)]
+  if (!is.null(order_variables)) {
+    if (all(vars %in% order_variables)) {
+      vars <- order_variables[which(order_variables %in% vars)]
       correlation.matrix <- correlation.matrix[vars, vars]
     } else {
-      stop("Not all variables of interest are included in order.variables argument.")
+      stop("Not all variables of interest are included in order_variables argument.")
     }
   }
 
@@ -119,22 +155,22 @@ plotNetwork <- function (covariance.results, variance.results = NULL, variance.t
 
   ## all metrics
 
-  networkMetrics <- data.frame(correlation_type = covariance.type,
+  networkMetrics <- data.frame(correlation_type = correlation_type,
                                EdgeDensity = EdgeDensity,
                                diameter = diameter,
                                averagePathLength = averagePathLength,
                                numberOfComponents = numberOfComponents,
                                averageClusteringCoefficient = averageClusteringCoefficient)
 
-  rownames(node.label) <- node.label[, 1]
-  node.label <- node.label[vars, 2]
+  rownames(node_label) <- node_label[, 1]
+  node_label <- node_label[vars, 2]
 
   ### Variance results (node pie chart) ####
 
-  if (!is.null(variance.results)) {
-    rownames(variance.results) <- variance.results[, 1]
-    if (!is.null(variance.results) && all(vars %in% rownames(variance.results))) {
-      ps.vars <- variance.results[vars, variance.type]
+  if (!is.null(variance_results)) {
+    rownames(variance_results) <- variance_results[, 1]
+    if (!is.null(variance_results) && all(vars %in% rownames(variance_results))) {
+      ps.vars <- variance_results[vars, variance_type]
       ps.vars[which(ps.vars < 0)] <- 0
       ps.vars[which(ps.vars > 1)] <- 1
     } else {
@@ -146,7 +182,7 @@ plotNetwork <- function (covariance.results, variance.results = NULL, variance.t
 
   ### Plot ####
 
-  if(displayDegreeAsNodeSize){
+  if(degree_as_node_size){
     nodeSize <- 5 + degree * 2
     nodeSize <- ifelse(nodeSize > 10, 10, nodeSize)
     nodeSize <- ifelse(nodeSize < 5, 5, nodeSize)
@@ -158,18 +194,18 @@ plotNetwork <- function (covariance.results, variance.results = NULL, variance.t
                       vsize = nodeSize, vsize2 = nodeSize, esize = 10 * max(abs(correlation.matrix)),
                       palette = "pastel", negDashed = T, borders = T, legend = F,
                       vTrans = 180, fade = F, aspect = T, legend.cex = 0.25,
-                      edge.labels = edge.label, edge.label.cex = 2 * length(vars)/length(vars),
-                      labels = node.label, label.cex = label.size, label.scale = F,
+                      edge_labels = edge_label, edge_label.cex = 2 * length(vars)/length(vars),
+                      labels = node_label, label.cex = label_size, label.scale = F,
                       node.label.offset = c(0.5, -3), pie = ps.vars, pieBorder = 1,
-                      DoNotPlot = F, groups = group.variables[, 2], threshold = threshold)
+                      DoNotPlot = F, groups = group_variables[, 2], correlation_threshold = correlation_threshold)
 
-  if(plot.metrics){
+  if(plot_metrics){
     graphics::text(x = 0.5, y = -1, labels = paste0("ED = ", round(networkMetrics$EdgeDensity, 2), "\n",
                                                     "NM = ", round(networkMetrics$numberOfComponents, 2), "\n",
                                                     "AC = ", round(networkMetrics$averageClusteringCoefficient, 2), "\n",
                                                     "|r|mean = ", round(networkMetrics$averagePathLength, 2), "\n",
                                                     "|r|max = ", round(networkMetrics$diameter, 2)),
-                   adj = 0, cex = networkMetrixTextSize)
+                   adj = 0, cex = network_metrics_text_size)
   }
   return(p)
 }
